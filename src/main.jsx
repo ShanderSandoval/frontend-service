c42a782862d7ae678ba37c0d94baf08cb8cb78e9
ShanderSandoval <sesandoval@uce.edu.ec>
2025-02-18 17:48:22 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import Router from "./router";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router />
   </StrictMode>
